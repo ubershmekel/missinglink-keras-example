56104aa90e3d1f7e7dcb1ab277a2ec90a16471b8
junhaoyap <lordyyap@gmail.com>
2017-09-20 20:50:49 +0800

    Some update

--- getting-started/mnist_experiment_hyperparams_missinglinkai.py
+++ getting-started/mnist_experiment_hyperparams_missinglinkai.py
@@ -32,8 +32,8 @@ ACTIVATION_RELU = 'relu'
 ACTIVATION_SOFTMAX = 'softmax'
 
 # Training params
-# EPOCHS = 8
-EPOCHS = 1
+DROPOUT_RATE = 0.1
+EPOCHS = 8
 BATCH_SIZE = 128
 CONV_DROPOUT = 0.25
 DENSE_DROPOUT = 0.5
@@ -107,7 +107,7 @@ missinglink_callback.set_properties(
     description='Two dimensional convolutional neural network')
 
 missinglink_callback.set_hyperparams(
-    total_epochs=EPOCHS)
+    dropout_rate=DROPOUT_RATE)
 
 model.fit(
     x_train, y_train, batch_size=BATCH_SIZE,
