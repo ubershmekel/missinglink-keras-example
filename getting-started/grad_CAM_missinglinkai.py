2ca734a4b3858d6986bbcb08ce2cdb3258853704
junhaoyap <lordyyap@gmail.com>
2017-10-30 17:47:01 +0800

    Update Keras grad cam

--- getting-started/grad_CAM_missinglinkai.py
+++ getting-started/grad_CAM_missinglinkai.py
@@ -7,8 +7,6 @@ missinglink_callback = missinglink.KerasCallback(
     owner_id='replace with owner id',
     project_token='replace with project token')
 
-missinglink_callback.set_properties(class_mapping={0:'dog', 1:'cat'})
-
 path = 'http://cmeimg-a.akamaihd.net/640/photos.demandstudios.com' + \
     '/getty/article/103/49/516464087.jpg'
 
